2705d7a0c9d8e4271b487a9cbbb4588c0c423995
orionstardust <justinono1990@outlook.com>
2022-08-17 23:11:43 -0700

    Update Card Facet

--- contracts/CardDiamond/facets/FakeGotchiCardFacet.sol
+++ contracts/CardDiamond/facets/FakeGotchiCardFacet.sol
@@ -20,25 +20,13 @@ contract FakeGotchiCardFacet is Modifiers {
      * @param _amount Amount to mint in this series
      */
     function startNewSeries(uint256 _amount) external onlyOwner {
-        require(_amount >= 0, "FGCard: Max amount must be greater than 0");
+        require(_amount > 0, "FGCard: Max amount must be greater than 0");
         uint256 newCardId = s.nextCardId;
         s.maxCards[newCardId] = _amount;
         LibERC1155._mint(address(this), newCardId, _amount, new bytes(0));
         s.nextCardId = newCardId + 1;
 
         emit NewSeriesStarted(newCardId, _amount);
-    }
-
-    // TODO: Replace with sale logic, Current function is for test purpose
-    function sale(address _to, uint256 _amount) external onlyOwner {
-        uint256 _id = s.nextCardId - 1;
-        address _from = address(this);
-        uint256 bal = s.cards[_from][_id];
-        require(_amount <= bal, "FGCard: Doesn't have that many to transfer");
-        s.cards[_from][_id] = bal - _amount;
-        s.cards[_to][_id] += _amount;
-        emit LibERC1155.TransferSingle(_from, _from, _to, _id, _amount);
-        LibERC1155.onERC1155Received(_from, _from, _to, _id, _amount, new bytes(0));
     }
 
     /**
@@ -74,13 +62,45 @@ contract FakeGotchiCardFacet is Modifiers {
     }
 
     /**
+     * @notice Transfers `_amount` of an `_id` from the diamond to the `_to` address specified (with safety call).
+     * @dev Caller must be approved to manage the tokens being transferred out of the diamond (see "Approval" section of the standard).
+     * Must contain scenario of internal _safeTransferFrom() function, _from should be diamond
+     * @param _to      Target address
+     * @param _id      ID of the token type
+     * @param _amount  Transfer amount
+     * @param _data    Additional data with no specified format, MUST be sent unaltered in call to `onERC1155Received` on `_to`
+     */
+    function safeTransferFromDiamond(
+        address _to,
+        uint256 _id,
+        uint256 _amount,
+        bytes calldata _data
+    ) external onlyOwner {
+        _safeTransferFrom(address(this), _to, _id, _amount, _data);
+    }
+
+    /**
+     * @notice Transfers `_amounts` of `_ids` from the diamond to the `_to` address specified (with safety call).
+     * @dev Caller must be approved to manage the tokens being transferred out of the diamond (see "Approval" section of the standard).
+     * Must contain scenario of internal _safeBatchTransferFrom() function, _from should be diamond
+     * @param _to      Target address
+     * @param _ids     IDs of each token type (order and length must match _amounts array)
+     * @param _amounts Transfer amounts per token type (order and length must match _ids array)
+     * @param _data    Additional data with no specified format, MUST be sent unaltered in call to the `ERC1155TokenReceiver` hook(s) on `_to`
+     */
+    function safeBatchTransferFromDiamond(
+        address _to,
+        uint256[] calldata _ids,
+        uint256[] calldata _amounts,
+        bytes calldata _data
+    ) external onlyOwner {
+        _safeBatchTransferFrom(address(this), _to, _ids, _amounts, _data);
+    }
+
+    /**
      * @notice Transfers `_amount` of an `_id` from the `_from` address to the `_to` address specified (with safety call).
      * @dev Caller must be approved to manage the tokens being transferred out of the `_from` account (see "Approval" section of the standard).
-     * MUST revert if `_to` is the zero address.
-     * MUST revert if balance of holder for token `_id` is lower than the `_amount` sent.
-     * MUST revert on any other error.
-     * MUST emit the `TransferSingle` event to reflect the balance change (see "Safe Transfer Rules" section of the standard).
-     * After the above conditions are met, this function MUST check if `_to` is a smart contract (e.g. code size > 0). If so, it MUST call `onERC1155Received` on `_to` and act appropriately (see "Safe Transfer Rules" section of the standard).
+     * Must contain scenario of internal _safeTransferFrom() function
      * @param _from    Source address
      * @param _to      Target address
      * @param _id      ID of the token type
@@ -94,27 +114,15 @@ contract FakeGotchiCardFacet is Modifiers {
         uint256 _amount,
         bytes calldata _data
     ) external {
-        require(_to != address(0), "FGCard: Can't transfer to 0 address");
         address sender = LibMeta.msgSender();
         require(sender == _from || s.operators[_from][sender] || sender == address(this), "FGCard: Not owner and not approved to transfer");
-        uint256 bal = s.cards[_from][_id];
-        require(_amount <= bal, "FGCard: Doesn't have that many to transfer");
-        s.cards[_from][_id] = bal - _amount;
-        s.cards[_to][_id] += _amount;
-        emit LibERC1155.TransferSingle(sender, _from, _to, _id, _amount);
-        LibERC1155.onERC1155Received(sender, _from, _to, _id, _amount, _data);
+        _safeTransferFrom(_from, _to, _id, _amount, _data);
     }
 
     /**
      * @notice Transfers `_amounts` of `_ids` from the `_from` address to the `_to` address specified (with safety call).
      * @dev Caller must be approved to manage the tokens being transferred out of the `_from` account (see "Approval" section of the standard).
-     * MUST revert if `_to` is the zero address.
-     * MUST revert if length of `_ids` is not the same as length of `_amounts`.
-     * MUST revert if any of the balance(s) of the holder(s) for token(s) in `_ids` is lower than the `_amounts` sent to the recipient.
-     * MUST revert on any other error.     *
-     * MUST emit `TransferSingle` or `TransferBatch` event(s) such that all the balance changes are reflected (see "Safe Transfer Rules" section of the standard).
-     * Balance changes and events MUST follow the ordering of the arrays (_ids[0]/_amounts[0] before _ids[1]/_amounts[1], etc).
-     * After the above conditions for the transfer(s) in the batch are met, this function MUST check if `_to` is a smart contract (e.g. code size > 0). If so, it MUST call the relevant `ERC1155TokenReceiver` hook(s) on `_to` and act appropriately (see "Safe Transfer Rules" section of the standard).
+     * Must contain scenario of internal _safeBatchTransferFrom() function
      * @param _from    Source address
      * @param _to      Target address
      * @param _ids     IDs of each token type (order and length must match _amounts array)
@@ -128,10 +136,68 @@ contract FakeGotchiCardFacet is Modifiers {
         uint256[] calldata _amounts,
         bytes calldata _data
     ) external {
-        require(_to != address(0), "FGCard: Can't transfer to 0 address");
-        require(_ids.length == _amounts.length, "FGCard: ids not same length as amounts");
         address sender = LibMeta.msgSender();
         require(sender == _from || s.operators[_from][sender], "FGCard: Not owner and not approved to transfer");
+        _safeBatchTransferFrom(_from, _to, _ids, _amounts, _data);
+    }
+
+    /**
+     * @notice Transfers `_amount` of an `_id` from the `_from` address to the `_to` address specified (with safety call).
+     * @dev
+     * MUST revert if `_to` is the zero address.
+     * MUST revert if balance of holder for token `_id` is lower than the `_amount` sent.
+     * MUST revert on any other error.
+     * MUST emit the `TransferSingle` event to reflect the balance change (see "Safe Transfer Rules" section of the standard).
+     * After the above conditions are met, this function MUST check if `_to` is a smart contract (e.g. code size > 0). If so, it MUST call `onERC1155Received` on `_to` and act appropriately (see "Safe Transfer Rules" section of the standard).
+     * @param _from    Source address
+     * @param _to      Target address
+     * @param _id      ID of the token type
+     * @param _amount  Transfer amount
+     * @param _data    Additional data with no specified format, MUST be sent unaltered in call to `onERC1155Received` on `_to`
+     */
+    function _safeTransferFrom(
+        address _from,
+        address _to,
+        uint256 _id,
+        uint256 _amount,
+        bytes calldata _data
+    ) internal {
+        require(_to != address(0), "FGCard: Can't transfer to 0 address");
+        address sender = LibMeta.msgSender();
+        uint256 bal = s.cards[_from][_id];
+        require(_amount <= bal, "FGCard: Doesn't have that many to transfer");
+        s.cards[_from][_id] = bal - _amount;
+        s.cards[_to][_id] += _amount;
+        emit LibERC1155.TransferSingle(sender, _from, _to, _id, _amount);
+        LibERC1155.onERC1155Received(sender, _from, _to, _id, _amount, _data);
+    }
+
+    /**
+     * @notice Transfers `_amounts` of `_ids` from the `_from` address to the `_to` address specified (with safety call).
+     * @dev
+     * MUST revert if `_to` is the zero address.
+     * MUST revert if length of `_ids` is not the same as length of `_amounts`.
+     * MUST revert if any of the balance(s) of the holder(s) for token(s) in `_ids` is lower than the `_amounts` sent to the recipient.
+     * MUST revert on any other error.     *
+     * MUST emit `TransferSingle` or `TransferBatch` event(s) such that all the balance changes are reflected (see "Safe Transfer Rules" section of the standard).
+     * Balance changes and events MUST follow the ordering of the arrays (_ids[0]/_amounts[0] before _ids[1]/_amounts[1], etc).
+     * After the above conditions for the transfer(s) in the batch are met, this function MUST check if `_to` is a smart contract (e.g. code size > 0). If so, it MUST call the relevant `ERC1155TokenReceiver` hook(s) on `_to` and act appropriately (see "Safe Transfer Rules" section of the standard).
+     * @param _from    Source address
+     * @param _to      Target address
+     * @param _ids     IDs of each token type (order and length must match _amounts array)
+     * @param _amounts Transfer amounts per token type (order and length must match _ids array)
+     * @param _data    Additional data with no specified format, MUST be sent unaltered in call to the `ERC1155TokenReceiver` hook(s) on `_to`
+     */
+    function _safeBatchTransferFrom(
+        address _from,
+        address _to,
+        uint256[] calldata _ids,
+        uint256[] calldata _amounts,
+        bytes calldata _data
+    ) internal {
+        require(_ids.length == _amounts.length, "FGCard: ids not same length as amounts");
+        require(_to != address(0), "FGCard: Can't transfer to 0 address");
+        address sender = LibMeta.msgSender();
         for (uint256 i; i < _ids.length; i++) {
             uint256 id = _ids[i];
             uint256 amount = _amounts[i];
