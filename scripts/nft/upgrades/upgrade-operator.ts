6bb1def48631c9e340e30c4ed02a70e3f463849c
Coderdan <dmathieuva@gmail.com>
2022-10-25 17:56:29 +0800

    Create upgrade-operator.ts

---
+++ scripts/nft/upgrades/upgrade-operator.ts
@@ -0,0 +1,49 @@
+import { ethers, run } from "hardhat";
+import { varsForNetwork } from "../../../constants";
+import {
+  convertFacetAndSelectorsToString,
+  DeployUpgradeTaskArgs,
+  FacetsAndAddSelectors,
+} from "../../../tasks/deployUpgrade";
+import { diamondOwner } from "../../helperFunctions";
+
+export async function upgrade() {
+  const MetadataInput =
+    "tuple(string name, string publisherName, string externalLink, string description, address artist, string artistName,uint16[2] royalty, uint16 editions, string fileHash, string fileType,string thumbnailHash, string thumbnailType)";
+
+  const facets: FacetsAndAddSelectors[] = [
+    {
+      facetName: "MetadataFacet",
+      addSelectors: [
+        `function addMetadataViaOperator(
+        ${MetadataInput} memory mData,uint256 series,address _operator) external`,
+        `function togglePublishingOperator(address _operator, bool _whitelist) external`,
+      ],
+      removeSelectors: [],
+    },
+  ];
+
+  const joined = convertFacetAndSelectorsToString(facets);
+
+  const c = await varsForNetwork(ethers);
+
+  const args: DeployUpgradeTaskArgs = {
+    diamondUpgrader: await diamondOwner(c.fakeGotchiArt, ethers),
+    diamondAddress: c.fakeGotchiArt,
+    facetsAndAddSelectors: joined,
+    useLedger: false,
+    useMultisig: false,
+  };
+
+  await run("deployUpgrade", args);
+}
+
+if (require.main === module) {
+  upgrade()
+    .then(() => process.exit(0))
+    // .then(() => console.log('upgrade completed') /* process.exit(0) */)
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
